74bd86fa939c39a5d4e2f06e6138b07109b634a4
automateyournetwork <ptcapo@gmail.com>
2025-04-19 07:58:11 -0400

    revert

--- selectorplus/selectorplus.py
+++ selectorplus/selectorplus.py
@@ -24,7 +24,6 @@ from langgraph.prebuilt.tool_node import ToolNode
 #from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
-from langchain_core.runnables import RunnableLambda
 
 A2A_PEER_AGENTS = os.getenv("A2A_PEER_AGENTS", "").split(",")
 
@@ -675,9 +674,8 @@ async def load_delegated_tools(peer_agents: Dict[str, dict]) -> List[Tool]:
     return delegated_tools
 
 embedding = OpenAIEmbeddings()
-embedding_runnable = RunnableLambda(lambda x: sync_embedding.embed_query(x))
 
-vector_store = InMemoryVectorStore(embedding=embedding_runnable)
+vector_store = InMemoryVectorStore(embedding=embedding)
 
 async def load_all_tools():
     """Async function to load tools from different MCP services and local files."""
